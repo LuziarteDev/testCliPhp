78b41220e221bb761f34064480fc1681643f8172
José Luziarte <jose.luziarte@agenciakindle.com.br>
2019-10-23 13:56:53 -0300

    Terminado todas as funçoes

--- res/functions.php
+++ res/functions.php
@@ -1,14 +1,19 @@
 <?php 
 function app(){
 
-sleep(1);
-render_menu_customer();
+    cls();
 
-sleep(1);
-render_option_menu();
+    sleep(1);
+    render_menu_customer();
+
+    sleep(1);
+    render_option_menu();
 
 }
 
+function cls(){
+    ( 'Linux' == PHP_OS) ? system('clear') : system('cls');
+}
 
 function render_header(){
     printf(" %s ", base64_decode('ICAgICAgICAgICAgICAgICAgICAgIC5fX19fX19fX19fXy4gX19fX19fXyAgICAgX19fX19fXy5fX19fX19fX19fXy4gX19fX19fXyAKfCAgICAgICAgICAgfHwgICBfX19ffCAgIC8gICAgICAgfCAgICAgICAgICAgfHwgICBfX19ffApgLS0tfCAgfC0tLS1gfCAgfF9fICAgICB8ICAgKC0tLS1gLS0tfCAgfC0tLS1gfCAgfF9fICAgCiAgICB8ICB8ICAgICB8ICAgX198ICAgICBcICAgXCAgICAgICB8ICB8ICAgICB8ICAgX198ICAKICAgIHwgIHwgICAgIHwgIHxfX19fLi0tLS0pICAgfCAgICAgIHwgIHwgICAgIHwgIHxfX19fIAogICAgfF9ffCAgICAgfF9fX19fX198X19fX19fXy8gICAgICAgfF9ffCAgICAgfF9fX19fX198CiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAKICAgICAgIC5fX19fX18gICAgICAgICAgX19fICAgICAgIF9fICAgX19fX19fX18gIAogICAgICAgfCAgIF8gIFwgICAgICAgIC8gICBcICAgICB8ICB8IHwgICAgICAgLyAgCiAgICAgICB8ICB8XykgIHwgICAgICAvICBeICBcICAgIHwgIHwgYC0tLS8gIC8gICAKICAgICAgIHwgICAgICAvICAgICAgLyAgL19cICBcICAgfCAgfCAgICAvICAvICAgIAogICAgICAgfCAgfFwgIFwtLS0tLi8gIF9fX19fICBcICB8ICB8ICAgLyAgLy0tLS0uCiAgICAgICB8IF98IGAuX19fX18vX18vICAgICBcX19cIHxfX3wgIC9fX19fX19fX3wKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIA==') );
@@ -20,8 +25,8 @@ function render_header(){
         sleep(1);
 
     }
-    ( 'Linux' == PHP_OS) ? system('clear') : system('cls');
 
+    cls();
 
     echo "\n=====================================: \n";
     echo "===    Sistema de beneficiário    ===: \n";
@@ -32,8 +37,9 @@ function render_header(){
 function render_menu_customer(){
     $menu = "
     1) Cadastrar novo\n
-    2) Listar todos cadastrados\n
+    2) Listar todos os planos cadastrados\n
     3) Preço total do Plano escolhido (soma do preço de cada beneficiário)\n
+    4) Sair\n
     ";
     echo $menu;
 }
@@ -72,6 +78,10 @@ function render_option_menu(){
         case '3':
             list_plans();
             get_total_plan();
+            break;
+
+        case '4':
+            echo "\nBYE BYE\n";
             break;
         
         default:
@@ -129,7 +139,7 @@ function new_customer(){
     $data = array();
 
     echo "\nCadastrando $qnt beneficiários(s)\n";
-    for ( $i = 0; $i < $qnt; $i++ ) { 
+    for ( $i = 0; $i < $qnt; $i++ ) {
 
         echo "\nDigite o nome do beneficiario\n";
         @$data[$i]->nome = readline();
@@ -137,7 +147,7 @@ function new_customer(){
         echo "\nDigite a idade do beneficiario\n";
         $age = readline();
         age_validate( $age );
-        @$data[$i]->idade = readline();
+        @$data[$i]->idade = $age;
 
         echo "\nSelecione um plano pelo código:\n";
         list_plans();
@@ -155,13 +165,80 @@ function new_customer(){
 }
 
 function age_validate( $age ){
-    if( $age < 0 || $age > 120 ){
-        echo "Idade inválida";
+    if( $age < 0 || $age > 120 || $age != is_numeric($age) ){
+        echo "Idade inválida\n";
         die();
     }
 }
 
 function get_total_plan(){
-    echo "\nSelecione qual plano deseja conferir os preços: \n";
+    echo "\nSelecione qual plano deseja conferir o preço total: \n";
     $option = readline();
+    $data = get_price_total_customer($option);
+
+    echo "\nPreço somado desse plano: R$" . number_format( $data['price_total'], 2, ',', ' ' );
+    
+    foreach( $data['all_customer'] as $key => $value ){
+        echo "\n---------------------\n";
+        echo "Nome do beneficiario: " . $value->nome;
+        echo "\nIdade do beneficiario: " . $value->idade;
+        echo "\nValor do plano para beneficiario: R$ " . number_format( $value->preco_do_plano, 2, ',', ' ' );
+        echo "\n";
+    }
+    readline('Pressione para continuar');
+    app();
+
+}
+
+function get_price_plan( $id_codigo_plano, $age ){
+    //Ler todos os beneficiarios da conta passado pelo ID e adiciona em $count_customer a quantidade de pessoas existente no plano
+    $customers = json_decode( file_get_contents('beneficiarios.json') );
+    $prices    = json_decode( file_get_contents('precos.json') );
+
+    $count_customer = 0;
+
+    foreach( $customers as $key => $value) if( $value->codigo_plano == $id_codigo_plano ) $count_customer++;
+    foreach( $prices as $key => $value ) if( $value->codigo == $id_codigo_plano ) $minimo_vida[] = $value;
+    foreach( $minimo_vida as $key => $value ) if( $count_customer >= $value->minimo_vidas ) $preco = $value;
+    
+	// - Pessoas de 0 a 17 anos vão para a faixa1.
+	// - Pessoas de 18 a 40 anos vão para a faixa2.
+	// - Pessoas com mais de 40 anos vão para a faixa3.
+    switch ( $age ) {
+        case ( $age > 0 && $age < 18 ):
+            return (float)$preco->faixa1;
+            break;
+            
+        case ( $age > 17 && $age < 41 ):
+            return (float)$preco->faixa2;
+            break;
+        
+        default:
+            return (float)$preco->faixa3;
+            break;
+    }
+
+}
+
+function get_price_total_customer( $id_codigo_plano ){
+    $customers = json_decode( file_get_contents('beneficiarios.json') );
+    $price_total = 0;
+    foreach( $customers as $key => $value ){
+        if( $value->codigo_plano == $id_codigo_plano ){
+            $price_total += get_price_plan( $id_codigo_plano, $value->idade );
+            $value->preco_do_plano = get_price_plan( $id_codigo_plano, $value->idade );
+            @$customer_total_price[] = $value;
+        }
+    }
+    if( empty( $customer_total_price ) || !isset( $customer_total_price ) ){
+        echo "\nNada cadastrado nesse plano\n";
+        sleep(1);
+        echo "\nPressione para continuar\n";
+        readline();
+        app();
+    }
+    return array(
+        "price_total"  => $price_total,
+        "all_customer" => $customer_total_price
+    );
 }
